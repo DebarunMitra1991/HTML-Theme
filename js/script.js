a22b63b69ca101292cecb9dc950d78cd5973c47a
DebarunMitra <debarun96mitra@gmail.com>
2020-01-19 12:26:37 +0530

    all animation applied

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
 $(document).ready(function(){
     $nav=$('.nav');
     $toggleCollapse = $('.toggle-collapse');
@@ -7,20 +22,23 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     });
 
-    //owl-carousel for blog
+    // owl-crousel for blog
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        autoplay:false,
-        autoplayTimeout:3000,
-        dots:false,
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
         nav: true,
-        navText: [$('.owl-navigation, owl-nav-prev'), $('.owl-navigation, owl-nav-next')]
-    }); 
-
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
     //click to move up
     $(".move-up span").click(function(){
         $("html, body").animate({
             scrollTop:0
         },1000);
     })
+
+    //css animation
+    AOS.init();
 }) 
